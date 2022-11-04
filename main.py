9b26241dd9ff8164a53e3cd6759904670ac87169
Jason Keith <jasonkeith5274@gmail.com>
2022-11-03 21:21:21 -0500

    set up new file and imported catalog courses

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
+import audit_functions
 
 def UploadAction(event=None):
     filename = filedialog.askopenfilename()
     print('Selected:', filename)
+
+    #audit_functions.audit_transcript()
+    
+
 
 root = tk.Tk()
 root.columnconfigure(0, weight=1)
